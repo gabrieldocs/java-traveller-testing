d6832d2a99e216b878e0b8f519648914959bd351
Lucas Gabriel Guilherme dos Santos <lucasggs.com@gmail.com>
2022-09-11 12:33:16 -0300

    feat: insere getter e setter para price em trip

--- src/main/java/work/gabrielsantos/app/Models/Trip.java
+++ src/main/java/work/gabrielsantos/app/Models/Trip.java
@@ -8,6 +8,7 @@ public class Trip {
     protected Date travel_end;
     protected City city_ini;
     protected City city_dest;
+    protected double price;
 
     public Trip(Client client, Date travel_ini, Date travel_end, City city_ini, City city_dest) {
         this.client = client;
@@ -15,5 +16,22 @@ public class Trip {
         this.travel_end = travel_end;
         this.city_ini = city_ini;
         this.city_dest = city_dest;
+    }
+
+    public Trip(Client client, Date travel_ini, Date travel_end, City city_ini, City city_dest, double price) {
+        this.client = client;
+        this.travel_ini = travel_ini;
+        this.travel_end = travel_end;
+        this.city_ini = city_ini;
+        this.city_dest = city_dest;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 }
